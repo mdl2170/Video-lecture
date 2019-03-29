540b7f4620d1cc95547a99ac4783122278a1ddbf
mdl2170 <31891494+mdl2170@users.noreply.github.com>
2019-03-29 13:35:34 -0400

    Add secret code

---
+++ Video lectures - Storyline output/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5kX5fonRdHm":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  alert("The piece of secret code: Academic");
+}
+
