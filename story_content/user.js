6b5d5e5915ad07e3e20e32eaf68189f8e8efa37d
mdl2170 <31891494+mdl2170@users.noreply.github.com>
2019-03-29 13:41:24 -0400

    Add secret code

--- story_content/user.js
+++ story_content/user.js
@@ -1,1 +1,15 @@
-// User defined code goes here
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5kX5fonRdHm":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  alert("The piece of secret code: Academic");
+}
+
